c149faa6ab7a4220e0b5e8bc5dee4ca1ba8448c6
Renata Persch <renatapersch@hotmail.com>
2022-08-08 18:52:19 -0300

    Formulário funcionando

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/cadastro/Formulario.java
@@ -0,0 +1,94 @@
+package br.edu.ifsp.scl.ads.pdm.cadastro;
+
+public class Formulario {
+
+    private String nome;
+    private String telefone;
+    private String email;
+    private Boolean lista;
+    private String sexo;
+    private String cidade;
+    private String uf;
+
+    public Formulario() {
+    }
+
+    public Formulario(String nome, String telefone, String email, Boolean lista, String sexo, String cidade, String uf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.lista = lista;
+        this.sexo = sexo;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getLista() {
+        return lista;
+    }
+
+    public void setLista(Boolean lista) {
+        this.lista = lista;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString() {
+        return "Formulario{" +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", lista=" + lista +
+                ", sexo=" + sexo +
+                ", cidade='" + cidade + '\'' +
+                ", uf='" + uf + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/cadastro/MainActivity.java
@@ -1,14 +1,55 @@
 package br.edu.ifsp.scl.ads.pdm.cadastro;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import br.edu.ifsp.scl.ads.pdm.cadastro.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding amb;
+    private Spinner spinner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(amb.getRoot());
+
+        spinner = findViewById(R.id.ufSp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ufs_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
+
+    public void limparDados(View view) {
+
+    }
+
+    public void salvarDados(View view) {
+        Formulario form = new Formulario();
+
+        form.setNome(amb.nomeEt.getText().toString());
+        form.setTelefone(amb.telefoneEt.getText().toString());
+        form.setEmail(amb.emailEt.getText().toString());
+        form.setLista(amb.listaEmailCb.isChecked());
+        form.setSexo(amb.femininoRb.isChecked() ? "Feminino" : "Masculino");
+        form.setCidade(amb.cidadeEt.getText().toString());
+        form.setUf(amb.ufSp.getSelectedItem().toString());
+
+        System.out.println(form);
+    }
+
 }
